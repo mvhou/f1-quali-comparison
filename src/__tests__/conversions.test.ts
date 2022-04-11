ddc98d2562265d7410023b65238622fc9f68734f
Mark van Hou <mvhou@Marks-MacBook-Air.local>
2022-04-11 14:09:52 +0200

    update conversion tests

--- src/__tests__/conversions.test.ts
+++ src/__tests__/conversions.test.ts
@@ -4,7 +4,7 @@ import { stringToMs, msToString } from '../test-utilities/conversion-tests'
 
 describe('string to ms conversion', () => {
     stringToMs.forEach((t:testInOut) => {
-        test(`conv.toMs(${t.input})`, () => {
+        test(`conv.toMs(${t.input}) should return ${t.output}`, () => {
             expect(conv.toMs(t.input)).toStrictEqual(t.output);
         });
     });
@@ -12,7 +12,7 @@ describe('string to ms conversion', () => {
 
 describe('ms to string conversion', () => {
     msToString.forEach((t:testInOut) => {
-        test(`conv.toString(${t.input})`, () => {
+        test(`conv.toString(${t.input}) should return ${t.output}`, () => {
             expect(conv.toString(t.input)).toStrictEqual(t.output);
         });
     });
